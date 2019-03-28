f5159b15880eb4bb6bd4bcdd3390f79b4368a180
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-28 19:23:55 +0200

    realize getPostById

--- src/main/java/com/divashchenko/ApiService.java
+++ src/main/java/com/divashchenko/ApiService.java
@@ -1,10 +1,23 @@
 package com.divashchenko;
 
+import com.divashchenko.Responses.Post;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class ApiService {
 
     private static ApiService instance;
 
+    private OkHttpClient client;
+    private String url;
+
     private ApiService() {
+        client = new OkHttpClient();
+        url = "https://jsonplaceholder.typicode.com/";
     }
 
     public static ApiService getInstance() {
@@ -12,5 +25,32 @@ public class ApiService {
             instance = new ApiService();
         }
         return instance;
+    }
+
+    public String takeJson(String additionalUrl) {
+        StringBuilder sb = new StringBuilder(url);
+        sb.append(additionalUrl);
+
+        Request request = new Request.Builder()
+                .url(sb.toString())
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Post getPostById(int id) {
+        StringBuilder sb = new StringBuilder("posts/");
+        sb.append(id);
+
+        String json = takeJson(sb.toString());
+
+        Gson gson = new Gson();
+        return gson.fromJson(json, Post.class);
     }
 }
