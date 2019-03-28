74952688b0376f5bafd4d569792ace8ec382ead5
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-28 19:24:02 +0200

    test getPostById

--- src/main/java/com/divashchenko/Main.java
+++ src/main/java/com/divashchenko/Main.java
@@ -1,7 +1,13 @@
 package com.divashchenko;
 
+import com.divashchenko.Responses.Post;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        ApiService service = ApiService.getInstance();
+
+        Post post = service.getPostById(1);
+
+        System.out.println(post);
     }
 }
