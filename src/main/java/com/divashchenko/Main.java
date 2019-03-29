0feb7299acaa889978152ed7c77cb0b33ffc5381
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-29 18:17:03 +0200

    Add tests

--- src/main/java/com/divashchenko/Main.java
+++ src/main/java/com/divashchenko/Main.java
@@ -2,12 +2,34 @@ package com.divashchenko;
 
 import com.divashchenko.Responses.Post;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ApiService service = ApiService.getInstance();
 
         Post post = service.getPostById(1);
-
         System.out.println(post);
+
+        System.out.println();
+        System.out.println("POST: " + service.postPost(post));
+        System.out.println("PUT: " + service.putPost(post));
+        System.out.println("DELETE: " + service.deletePost(post));
+        System.out.println();
+
+        System.out.println("GET /comments?postId=1 : ");
+        System.out.println();
+        System.out.println(service.getCommentsFromPost(1));
+        System.out.println();
+
+        System.out.println("GET /posts?userId=1 : ");
+        System.out.println();
+        System.out.println(service.getPostsFromUser(1));
+        System.out.println();
+
+        System.out.println("Get Posts:");
+        System.out.println();
+        List<Post> posts = service.getPosts();
+        System.out.println(posts);
     }
 }
